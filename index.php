313f8edfac7a1daa539801e11653f17496befd68
GiuliaSossai <giulia.sossai@yahoo.it>
2021-12-22 12:59:21 +0100

    init project - milestone 1

---
+++ index.php
@@ -0,0 +1,69 @@
+<?php
+
+require_once __DIR__ . "/db.php";
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+   <meta charset="UTF-8">
+   <meta http-equiv="X-UA-Compatible" content="IE=edge">
+   <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+
+   <link rel="stylesheet" href="css/style.css">
+
+   <title>Dischi php</title>
+</head>
+
+<body>
+   <div id="app">
+      <!-- header -->
+      <header >
+         <div class="container-fluid d-flex justify-content-between">
+            <div class="logo-img">
+               <img src="./img/logo.png" alt="">
+            </div>
+
+            <select>
+               <option value="Tutti">Tutti</option>
+               <option value="Rock">Rock</option>
+               <option value="Pop">Pop</option>
+               <option value="Jazz">Jazz</option>
+               <option value="Metal">Metal</option>
+            </select>
+         </div>
+      </header>
+
+      <!-- main -->
+      <main>
+         <div class="container">
+            <div class="row">
+               <!-- disco singolo -->
+               <?php foreach($albums as $album) {?>
+               <div class="text-center mb-5 py-3 gs-album">
+                  <img src="<?php echo $album['poster'] ?>" alt="<?php echo $album['title'] ?>">
+                  <h4 class="text-uppercase font-weight-bold my-2"><?php echo $album['title'] ?></h4>
+                  <p class="m-0"><?php echo $album['author'] ?></p>
+                  <p class="m-0"><?php echo $album['year'] ?></p>
+              </div>
+              <?php } ?> 
+
+            </div>  
+         </div>
+          
+      </main>
+      
+      
+   </div>
+
+   <!-- vue -->
+   <script src="https://cdn.jsdelivr.net/npm/vue@2/dist/vue.js"></script>
+   <!-- axios -->
+   <script src="https://cdn.jsdelivr.net/npm/axios/dist/axios.min.js"></script>
+   <!-- mio js -->
+   <script src="vue.js"></script>
+</body>
+</html>
---
+++ vue.js
@@ -0,0 +1,44 @@
+const app = new Vue ({
+   el: '#app',
+
+   data: {
+      // errorMsg: false,
+      // mail: '',
+      // emails: [],
+      // isLoading: true
+   },
+
+   methods: {
+      // getMail(){
+         
+      //    for(let i = 0; i < 10; i++){
+      //       axios.get('https://flynn.boolean.careers/exercises/api/random/mail')
+      //          .then((response) =>{
+      //             //console.log('intero oggetto', response);
+
+      //             const data = response.data;
+      //             //console.log('oggetto data:', data);
+      //             console.log('mail ottenuta', data.response);
+
+      //             this.mail = data.response;
+
+      //             this.emails.push(this.mail);
+      //             console.log('array emails', this.emails);
+      //             this.isLoading = false;
+      //          })
+
+      //          .catch((error) => {
+      //             console.log(error);
+      //             this.errorMsg = true;
+      //          })      
+      //    }
+         
+      // }
+
+   },
+
+   mounted(){
+      //this.getMail();
+   }
+
+})
