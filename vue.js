3b926f5fd3ce27fdd8886b654b8388a836ec792e
GiuliaSossai <giulia.sossai@yahoo.it>
2022-01-06 10:36:39 +0100

    chiamata api con axios

--- vue.js
+++ vue.js
@@ -2,43 +2,28 @@ const app = new Vue ({
    el: '#app',
 
    data: {
-      // errorMsg: false,
-      // mail: '',
-      // emails: [],
-      // isLoading: true
+      db: "./db.php",
+      discs: [],
    },
 
    methods: {
-      // getMail(){
-         
-      //    for(let i = 0; i < 10; i++){
-      //       axios.get('https://flynn.boolean.careers/exercises/api/random/mail')
-      //          .then((response) =>{
-      //             //console.log('intero oggetto', response);
+      getApi(){ 
+         axios.get(this.db)
+         .then(r =>{
+             this.discs = r.data;
+             // console.log(this.discs);
+             this.option();
 
-      //             const data = response.data;
-      //             //console.log('oggetto data:', data);
-      //             console.log('mail ottenuta', data.response);
-
-      //             this.mail = data.response;
-
-      //             this.emails.push(this.mail);
-      //             console.log('array emails', this.emails);
-      //             this.isLoading = false;
-      //          })
-
-      //          .catch((error) => {
-      //             console.log(error);
-      //             this.errorMsg = true;
-      //          })      
-      //    }
-         
-      // }
+         })
+         .catch(e =>{
+             console.log(e);
+         })
+     },
 
    },
 
    mounted(){
-      //this.getMail();
+      this.getApi();
    }
 
 })
